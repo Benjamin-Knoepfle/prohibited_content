6823f5f2d9f898659d8fdbd231eba864fb223354
Benjamin-Knoepfle <borschi@gmail.com>
2014-08-07 16:27:24 +0200

    changes for the weekend

--- raw_code/driver.py
+++ raw_code/driver.py
@@ -3,11 +3,15 @@ import prohibited_code as pc
 
 from sklearn import svm
 from sklearn.linear_model import SGDClassifier
+from sklearn.neighbors import KNeighborsClassifier
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.linear_model import Perceptron
 from sklearn.metrics import average_precision_score
 
 import execnet, remote_preprocess
 import cPickle as pickle
 import itertools
+import math
 
 import datetime
 
@@ -15,7 +19,10 @@ import datetime
 file_path = '../../data/raw_data/'
 train_file_name = 'avito_train.tsv'
 test_file_name = 'avito_test.tsv'
-sample_size = 20000
+train_max_samples = 3995802
+test_max_samples = 1351242
+
+sample_size = 5000
 
 
 #train
@@ -46,21 +53,29 @@ mch = execnet.MultiChannel( [ channel1, channel2, channel3 ] )
 queue = mch.make_receive_queue( endmarker=-1 )
 channels = itertools.cycle( mch )
 
-num_splits = 4
+num_splits = 3
+
+messages = []
+for i in range( num_splits ):
+        start_pos = i*sample_size
+        if start_pos > train_max_samples: continue
+        messages.append( (file_path+train_file_name, sample_size, start_pos, True) )
+
 
 print "send data"
-for i in range( num_splits ):
-        start_pos = 4000000#i*sample_size
+for message in messages:
+        
         channel = channels.next()
-        channel.send( (file_path+train_file_name, sample_size, start_pos, True) )
+        channel.send( message )
 
 train_data_sets=[]
 train_label_sets=[]
 
 
 print "receive data"
-for i in range( num_splits ):
+for i in range( len( messages ) ):
         channel, serialized_data = queue.get( )
+        print "Split number " +str(i)+ " received"
         if serialized_data == -1: print "Error with data;" 
         train_data, labels = pickle.loads( serialized_data )
         train_data_sets.append( train_data )
@@ -72,36 +87,57 @@ print "tidy data"
 feature_index = {}
 item_ids, train_feat, labels = pc.tidy_data( train_data_sets, train_label_sets, feature_index = feature_index )
 
-train_set, test_set, evaluation_set = pc.create_train_sets( train_feat, labels, train_frc=0.75, test_frc=0.2, evaluation_frq=0, method='simple' )
+train_set, test_set, evaluation_set = pc.create_train_sets( train_feat, labels, train_frc=1, test_frc=0, evaluation_frq=0, method='simple' )
 
 print "train classifier"
 clf = svm.SVC()
+
 clf.fit( train_set['data'], train_set['labels'] )
-print "evaluate classifier"
-test_prediction = clf.predict( test_set['data'] )
-test_score = average_precision_score( test_set['labels'], test_prediction )
-print "score: " + str( test_score )
 
-group.terminate()
+if(len( test_set )>0):
+        print "evaluate classifier"
+        test_prediction = clf.predict( test_set['data'] )
+        test_score = average_precision_score( test_set['labels'], test_prediction )
+        print "score: " + str( test_score )
 
-'''
+
+####################################################################################
+##      This is the classification tast                                           ##
+####################################################################################
+
 #test
 print "test"
 
 print "preprocess test"
+
+print "calculate number of splits for test_data"
+num_splits = test_max_samples/sample_size +1
+print str(num_splits) + " splits needed for sample_size " + str(sample_size)
+
+messages = []
+for i in range( num_splits ):
+        start_pos = i*sample_size
+        if start_pos > train_max_samples: continue
+        messages.append( (file_path+test_file_name, sample_size, start_pos, False) )
+
+
 print "send data"
-for i in range(3):
-        start_pos = 0
+for message in messages:
+        
         channel = channels.next()
-        channel.send( (file_path+test_file_name, sample_size, start_pos, False) )
+        channel.send( message )
 
 test_data_sets=[]
 
+
 print "receive data"
-for i in range(3):
-        channel, serialized_data = queue.get()
+for i in range( len( messages ) ):
+        channel, serialized_data = queue.get( )
+        print "Split number " +str(i)+ " received"
+        if serialized_data == -1: print "Error with data;" 
         test_data = pickle.loads( serialized_data )
         test_data_sets.append( test_data )
+
 
 group.terminate()
 #test_data = pc.preprocess_data2( file_path+test_file_name, range(sample_size), targets=False )
@@ -110,10 +146,10 @@ print "tidy data"
 item_ids, test_feat = pc.tidy_data( test_data_sets, feature_index = feature_index )
 
 print "classify"
-#predicted_scores = clf.predict( test_feat )
+predicted_scores = clf.predict( test_feat )
 
-#solution =  pc.sort_solution( item_ids, predicted_scores )
+solution =  pc.sort_solution( item_ids, predicted_scores )
 
-#solution_file_name = '../../data/data/solution' + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + '.csv'
-#pc.write_solution( solution_file_name, solution )
-'''
+solution_file_name = '../../data/data/solution' + datetime.datetime.now().strftime("%Y-%m-%d_%H:%M:%S") + '.csv'
+pc.write_solution( solution_file_name, solution )
+
