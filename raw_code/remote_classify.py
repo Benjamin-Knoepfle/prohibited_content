3807f24037c170a46db348735f5ebbeb35a1cbac
Benjamin-Knoepfle <borschi@gmail.com>
2014-08-10 10:57:22 +0200

    classification parallel, tidy data parallel v2

---
+++ raw_code/remote_classify.py
@@ -0,0 +1,19 @@
+
+import cPickle as pickle
+
+if __name__ == '__channelexec__':
+        
+        try:
+		clf = pickle.loads( channel.receive() )
+                classifier = pickle.loads( channel.receive() )
+
+                for param in channel:
+                        file_name = param[0]
+                        sample_size = param[1]
+                        start_pos = param[2]
+                        targets = param[3]
+                        classified = classifier( clf, file_name, sample_size, start_pos, targets )
+                        serialized_classified = pickle.dumps( classified )
+                        channel.send( serialized_classified )
+        except( BaseException ):
+                channel.send( -1 )
